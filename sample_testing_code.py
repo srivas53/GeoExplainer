8f03e18914d9aa1c7cbd2270b0bd86a6c823e7b2
Varun Srivastava <vsriva11@sundevils.asu.edu>
2022-05-18 14:56:35 -0700

    Added new layers display

--- sample_testing_code.py
+++ sample_testing_code.py
@@ -41,15 +41,17 @@ finalCenter
 
     
 
+#MT Parcel Layer Data: 
 #Establish a connecton to Mongo
 
 
 from pymongo import MongoClient
-import pandas as pd
 import geopandas as gp
 from shapely.geometry import MultiPoint
 from shapely.geometry import shape
-import json
+import mapclassify
+import pandas as pd
+import numpy as np
 
 def _connect_mongo(host, port, username, password, db):
     """ A util for making a connection to mongo """
@@ -91,12 +93,17 @@ montanaData = read_mongo("eco_region", "MTParcel_layer1and2")
 
 #montanaData2 = montanaData.iloc[0:4000,]
 
+#Convert JSON type to Geometry data type
 
 for row in montanaData.itertuples():
     montanaData.at[row.Index, "geometry"] = shape(row.geometry)
     #row.geometry = shape(row.geometry)
     #listOfCentroids.append((row.x,row.y))
-    
+
+
+###Written for testing while extrcting cnetroid information, and to sort data later on    ##############
+
+
 montanaData['x'] = ''
 montanaData['y'] = ''
 issue_id = []
@@ -110,22 +117,7 @@ for row in montanaData.itertuples():
         continue
         
         
-    
-# sampleGeojson['x']=sampleGeojson["geometry"].centroid.x
-# sampleGeojson['y']=sampleGeojson["geometry"].centroid.y
 
-# listOfCentroids = []
-
-
-# for row in sampleGeojson.itertuples():
-#     listOfCentroids.append((row.x,row.y))
-
-# finalPoints = MultiPoint(listOfCentroids)
-# print(finalPoints.centroid.x)
-# print(finalPoints.centroid.y)
-
-# finalCenter = [finalPoints.centroid.x,finalPoints.centroid.y]
-#sampleGeojson.to_file('user_session_polygon.geojson', driver='GeoJSON')
 
 #sortColumn
 montanaData['sortColumn'] = montanaData['x'] + montanaData['y']
@@ -133,26 +125,88 @@ montanaData = montanaData.sort_values('sortColumn',ascending = 'True')
 
 montanaDataSubSet = montanaData.iloc[0:4000,]
 
-montanaDataSubSet['PARCELID'] = ''
-montanaDataSubSet['SHAPE_Leng'] = ''
-montanaDataSubSet['roadAver'] = ''
-montanaDataSubSet['cost'] = ''
+montanaDataSubSet['PARCELID'] = None
+montanaDataSubSet['SHAPE_Leng'] = None
+montanaDataSubSet['roadAver'] = None
+montanaDataSubSet['cost'] = None
+montanaDataSubSet['area'] = None
+#montanaDataSubSet['center'] = None
+montanaDataSubSet['averagebird'] = None
+montanaDataSubSet['averagefish'] = None
+montanaDataSubSet['averagereptile'] = None
+montanaDataSubSet['averagetree'] = None
+montanaDataSubSet['averageamphibian'] = None
+montanaDataSubSet['costAver'] = None
+montanaDataSubSet['layer1paDist'] = None
+montanaDataSubSet['layer2paDist'] = None
+montanaDataSubSet['paAver'] = None
+montanaDataSubSet['averHyDist'] = None
+montanaDataSubSet['averagemammal'] = None
+
+issue_id_2 = []
+montanaDataSubSet = montanaDataSubSet.reset_index(drop = True)
 
 for row in montanaDataSubSet.itertuples():
-    montanaDataSubSet.at[row.Index, "PARCELID"] = row.properties['PARCELID']
-    montanaDataSubSet.at[row.Index, "SHAPE_Leng"] = row.properties['SHAPE_Leng']
-    montanaDataSubSet.at[row.Index, "roadAver"] = row.properties['roadAver']
-    montanaDataSubSet.at[row.Index, "cost"] = row.properties['cost']
+    try:
+        montanaDataSubSet.at[row.Index, "PARCELID"] = row.properties['PARCELID']
+        montanaDataSubSet.at[row.Index, "SHAPE_Leng"] = row.properties['SHAPE_Leng']
+        montanaDataSubSet.at[row.Index, "roadAver"] = row.properties['roadAver']
+        montanaDataSubSet.at[row.Index, "cost"] = row.properties['cost']
+        montanaDataSubSet.at[row.Index, "area"] = row.properties['area']
+        #montanaDataSubSet.at[row.Index, "center"] = row.properties['center']
+        montanaDataSubSet.at[row.Index, "averagebird"] = row.properties['averagebird']
+        montanaDataSubSet.at[row.Index, "averagefish"] = row.properties['averagefish'] 
+        montanaDataSubSet.at[row.Index, "averagereptile"] = row.properties['averagereptile']
+        montanaDataSubSet.at[row.Index, "averagetree"] = row.properties['averagetree']
+        montanaDataSubSet.at[row.Index, "averageamphibian"] = row.properties['averageamphibian']
+        montanaDataSubSet.at[row.Index, "costAver"] = row.properties['costAver']      
+        montanaDataSubSet.at[row.Index, "layer1paDist"] = row.properties['layer1paDist']
+        montanaDataSubSet.at[row.Index, "layer2paDist"] = row.properties['layer2paDist']
+        montanaDataSubSet.at[row.Index, "paAver"] = row.properties['paAver']          
+        montanaDataSubSet.at[row.Index, "averHyDist"] = row.properties['averHyDist'] 
+        montanaDataSubSet.at[row.Index, "averagemammal"] = row.properties['averagemammal'] 
+    except KeyError:
+        issue_id_2.append(row.Index)
+        continue        
+        
 
+
+#montanaDataSubSet = montanaDataSubSet.fillna(method='ffill',axis = 1)
 montanaDataSubSet['cost'] = montanaDataSubSet['cost'].astype(int)
 montanaDataSubSet['roadAver'] = montanaDataSubSet['roadAver'].astype(float)
 montanaDataSubSet['SHAPE_Leng'] = montanaDataSubSet['SHAPE_Leng'].astype(float)
+montanaDataSubSet['area'] = montanaDataSubSet['area'].astype(float)
+montanaDataSubSet["averagebird"] = montanaDataSubSet['averagebird'].astype(float)
+montanaDataSubSet["averagefish"] = montanaDataSubSet['averagefish'].astype(float)
+montanaDataSubSet["averagereptile"] = montanaDataSubSet['averagereptile'].astype(float)
+montanaDataSubSet["averagetree"] = montanaDataSubSet['averagetree'].astype(float)
+montanaDataSubSet["averageamphibian"] = montanaDataSubSet['averageamphibian'].astype(float)
+montanaDataSubSet["costAver"] = montanaDataSubSet['costAver'].astype(float)
+montanaDataSubSet["layer1paDist"] = montanaDataSubSet['layer1paDist'].astype(float)
+montanaDataSubSet["layer2paDist"] = montanaDataSubSet['layer2paDist'].astype(float)
+montanaDataSubSet["paAver"] = montanaDataSubSet['paAver'].astype(float)    
+montanaDataSubSet["averHyDist"] = montanaDataSubSet['averHyDist'].astype(float)  
+montanaDataSubSet["averagemammal"] = montanaDataSubSet['averagemammal'].astype(float)  
+ 
+
+#montanaDataSubSet['averagefish'] = montanaDataSubSet['averagefish'].interpolate()
+
 
 montanaDataSubSet[['geometry',
  'PARCELID',
  'SHAPE_Leng',
  'roadAver',
- 'cost']].to_file('MT_Parcel_Layer_Sample.geojson', driver='GeoJSON')
+ 'cost', 'area',
+ 'averagebird',
+ 'averagefish',
+ 'averagereptile',
+ 'averagetree',
+ 'averageamphibian',
+ 'costAver',
+ 'layer1paDist',
+ 'layer2paDist',
+ 'paAver',
+ 'averHyDist','averagemammal']].to_file('MT_Parcel_Layer_Sample.geojson', driver='GeoJSON')
 
 #Make this work for Mapbox
 
@@ -160,6 +214,11 @@ montanaDataSubSet[['geometry',
 sampleGeojson = gp.read_file(r"C:\Users\vsriva11\Desktop\VADER Lab\GeoExplainer\static\data\MT_Parcel_Layer_Sample.geojson")
 
 
+#Classes for "Cost" layer choropleth map
+
+fj5 = mapclassify.FisherJenks(sampleGeojson['cost'], k=3)
+sampleGeojson["averagetree"] = sampleGeojson["averagetree"].replace(np.nan, -1000)
+fj3 = mapclassify.FisherJenks(sampleGeojson['averagetree'], k=5)
 
 
 
@@ -170,10 +229,7 @@ sampleGeojson = gp.read_file(r"C:\Users\vsriva11\Desktop\VADER Lab\GeoExplainer\
 
 
 
-
-
-
-
+#averagemammal
 
 
 
